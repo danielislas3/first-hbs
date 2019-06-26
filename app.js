794af15f2af71792e7b9b57778335a80e20a8adb
Daniel Orio <danielislas3@live.com>
2019-06-26 11:09:10 -0500

    done

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express= require('express')
 const app = express()
 
+
 //asignamos el motor de vistas
+app.use(express.static('public'))
 app.set('views',`${__dirname}/views`)
 app.set('view engine','hbs')
 
 
 app.get('/',(req,res)=>{
-  //TODO completar ruta
-  res.render('index')
+  let data={
+    name: "Ironhacker",
+    bootcamp: "<span>IronHack WebDev</span>",
+    lastname:"Yolo",
+    address: {
+      street: "Your heart",
+      number: 87
+    },
+    cities: ["Miami", "Madrid", "Barcelona", "Paris", "México", "Berlín"]
+  }
+  res.render('index',data)
 })
+
+
 app.get('/about',(req,res)=>{
   //TODO completar ruta
   res.render('about')
